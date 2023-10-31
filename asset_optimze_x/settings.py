88903a485bd8aa3635e7f097ce9253ec6208bab3
abdul <abdulhasib2030@gmail.com>
2023-10-31 21:11:06 +0600

    add payment

--- asset_optimze_x/settings.py
+++ asset_optimze_x/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hf&lhhx^%rq28-9^nnbb2==88h5_4v^@ruytq^_s9x$0%-6p22'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'True') == "True"
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', "assetoptimizex.onrender.com"]
 
