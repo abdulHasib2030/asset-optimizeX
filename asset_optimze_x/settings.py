bf633985d2b5bac24eed894a234081f6a6ad395a
abdul <abdulhasib2030@gmail.com>
2023-10-31 16:33:19 +0600

    change settings

--- asset_optimze_x/settings.py
+++ asset_optimze_x/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-hf&lhhx^%rq28-9^nnbb2==88h5_4v^@ruytq^_s9x$0%-6p22
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == "True"
 
-ALLOWED_HOSTS = ['127.0.01', 'localhost', "assetoptimizex.onrender.com"]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', "assetoptimizex.onrender.com"]
 
 
 # Application definition
